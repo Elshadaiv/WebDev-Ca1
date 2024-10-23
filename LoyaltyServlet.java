0c3d6d50718c9c94bb5b5565525762582d6b3f89
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-10-23 14:51:04 +0100

    Update LoyaltyServlet.java
    First start to my assignment

--- LoyaltyServlet.java
+++ LoyaltyServlet.java
@@ -1,4 +1,13 @@
 
-public class LoyaltyServlet {
-
+public class LoyaltyServlet extends HttpServlet {
+public void doGet(HttpServletRequest requst, HttpServletResponse respone) throws ServletException, IOException
+	{
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC","root", "root");
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 }
