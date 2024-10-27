c8e8f08cea40fd9220da5af03578ace7844a9917
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-10-27 22:28:45 +0000

    Update PointsServlet.java

--- PointsServlet.java
+++ PointsServlet.java
@@ -1,4 +1,20 @@
 
 public class PointsServlet {
-
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException
+	{
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC","root", "root");
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		String username = request.getParameter("Username");
+		int AddPoints = request.getParameter("AddPoints");
+		int SpendPoints = request.getParameter("SpendPoints");
+		PreparedStatement getUserPoints = connection.prepareStatement("SELECT Points FROM user WHERE Username = ?");
+		getUserPoints.setString(1, username);
+	}
 }
