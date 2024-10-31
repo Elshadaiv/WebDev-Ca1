7134a13398301ab3e26dc9d3629ed1669ff0569e
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-10-31 22:14:24 +0000

    Update LoyaltyServlet.java
    updated code as it had snythax errors

--- LoyaltyServlet.java
+++ LoyaltyServlet.java
@@ -1,17 +1,29 @@
 
 public class LoyaltyServlet extends HttpServlet {
-public void doGet(HttpServletRequest requst, HttpServletResponse respone) throws ServletException, IOException
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		Connection connection = null;
 		
 		try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC","root", "root");
-		} catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		try {
-			if(connection != null && request.getParameter("Password1").equals(request.getParameter("Password2")));
+		
+		
+		
+			String username = request.getParameter("Username");
+			String password1 = request.getParameter("Password1");
+			String password2 = request.getParameter("Password2");
+			
+			if(username == null || password1 == null || password2 == null)
+			{
+				response.getWriter().println("You must enter all fields");
+			}
+			
+			if(!password1.equals(password2))
+			{
+				response.getWriter().println ("Password doesn't match");
+			}
+
+			if(connection != null && password1.equals(password2)) {
 			PreparedStatement createUser = connection.prepareStatement(
 					"INSERT into User "
 							+ "(Username, Password, Points)" +" VALUES (?, ?, ?)");
@@ -30,19 +42,15 @@ public void doGet(HttpServletRequest requst, HttpServletResponse respone) throws
 			{
 				response.getWriter().println("Error, Please try again");
 			}
-		
+			}
 		} catch (SQLException e1) 
 		{
 			e1.printStackTrace();
 		}
+	
 
-		
-		if (request.getParameter("Password")!=(request.getParameter("Password2")));
-		{
-			response.getWriter().println("Password doesn't match, Please try again");
-
-		}
 			
+	}
 	}
 
 }
