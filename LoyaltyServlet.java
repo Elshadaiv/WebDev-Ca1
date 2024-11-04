9f936a7d7187ae45192f4b905a2f50788e40ff89
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-11-04 12:49:37 +0000

    Update LoyaltyServlet.java
    updated code

--- LoyaltyServlet.java
+++ LoyaltyServlet.java
@@ -18,37 +18,38 @@ public class LoyaltyServlet extends HttpServlet
 		Connection connection = null;
 		
 		try {
+			//the connection to the databaase
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC","root", "root");
 		
 		
-		
+			// declaring the username password 
 			String username = request.getParameter("Username");
 			String password1 = request.getParameter("Password1");
 			String password2 = request.getParameter("Password2");
 			
 			if(username == null || password1 == null || password2 == null || username.isEmpty() || password1.isEmpty() || password2.isEmpty())
 			{
-				response.getWriter().println("You must enter all fields");
+				response.getWriter().println("You must enter all fields"); /// this message comes up if user doesnt enter all the fields when pressing register
 			}
 			
 			if(!password1.equals(password2))
 			{
-				response.getWriter().println ("Password doesn't match");
+				response.getWriter().println ("Password doesn't match"); // this message appears when teh user passsword 1 doesnt match password 2
 			}
 
 			if(connection != null && password1.equals(password2)) {
 			PreparedStatement createUser = connection.prepareStatement(
 					"INSERT into User "
-							+ "(Username, Password, Points)" +" VALUES (?, ?, ?)");
-			createUser.setString(1, request.getParameter("Username"));
+							+ "(Username, Password, Points)" +" VALUES (?, ?, ?)"); // adding it to the database
+			createUser.setString(1, request.getParameter("Username")); 
 			createUser.setString(2, request.getParameter("Password1"));
-			createUser.setInt(3, 100);
-			int rowsUpdated = createUser.executeUpdate();
+			createUser.setInt(3, 100); // points to 100 for users
+			int rowsUpdated = createUser.executeUpdate(); // stores 
 			createUser.close();
 			
 			if (rowsUpdated > 0)
 			{
-				response.sendRedirect("Login.html");
+				response.sendRedirect("Login.html"); // this redirects to the login html page
 			}
 			
 			else 
@@ -65,6 +66,10 @@ public class LoyaltyServlet extends HttpServlet
 			
 	}
 	}
+
+
+
+
 
 
 
