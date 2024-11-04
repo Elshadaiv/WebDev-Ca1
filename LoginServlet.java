c582d4a8b83af4dff8102111a54d2f94ba277a7b
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-11-04 12:49:08 +0000

    Update LoginServlet.java
    updated changes its now working

--- LoginServlet.java
+++ LoginServlet.java
@@ -12,26 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("Username");
-        String password = request.getParameter("Password");
-        response.setContentType("text/html");
+        String password = request.getParameter("Password"); //http request
+        response.setContentType("text/html"); // respond to html
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC", "root", "root")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Loyalty?serverTimezone=UTC", "root", "root")) 
+        {
       
             PreparedStatement PreparedStatement = connection.prepareStatement("SELECT Points FROM User WHERE Username = ? AND Password = ?");
             PreparedStatement.setString(1, username);
             PreparedStatement.setString(2, password);
             ResultSet resultSet = PreparedStatement.executeQuery();
 
-            if (resultSet.next()) {
+            if (resultSet.next())
+            {
                 int points = resultSet.getInt("Points");
-                response.sendRedirect("/LoyaltyApp/loyaltyHomePage.html");
-            } else {
+                response.sendRedirect("LoyaltyHomePage.html"); //if login successful this message appears
+            } else 
+            {
                 PrintWriter out = response.getWriter();
-                out.println("<html><body><h3>Invalid username or password.</h3></body></html>");
+                out.println("<html><body><h3>Invalid username or password.</h3></body></html>"); //if not this message appears
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.getWriter().println("<html><body><h3>Error occurred: " + e.getMessage() + "</h3></body></html>");
+        } catch (Exception e1) 
+        {
+            e1.printStackTrace();
+   
         }
     }
 }
